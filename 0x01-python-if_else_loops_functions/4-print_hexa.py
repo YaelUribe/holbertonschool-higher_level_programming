82609d34b1e744e316478baa9837d9c400284d8a
Yaeluribe <2962@holbertonschool.com>
2021-04-27 17:40:33 -0500

    Numbers from 00 to 99

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-    print("{} = {}".format(x, hex(x)))
+for x in range(0, 100):
+    if x < 99:
+        print("{:02d}, ".format(x), end="")
+    else:
+        print("{}".format(x))
