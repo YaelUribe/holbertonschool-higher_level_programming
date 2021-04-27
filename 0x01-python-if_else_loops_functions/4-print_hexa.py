f12c134b4376cf950409536369fe4acece75790d
Yaeluribe <2962@holbertonschool.com>
2021-04-27 17:41:57 -0500

    hexadecimal representations

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for x in range(0, 100):
-    if x < 99:
-        print("{:02d}, ".format(x), end="")
-    else:
-        print("{}".format(x))
+for x in range(97, 123):
+    if x == 101 or x == 113:
+        continue
+    print("{}".format(chr(x)), end="")
