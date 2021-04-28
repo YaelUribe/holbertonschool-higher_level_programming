4375b74578b1c9ef3ccdf4ee333e2c3bb1e57b33
Yaeluribe <2962@holbertonschool.com>
2021-04-27 22:06:26 -0500

    FizzBuzz ... again

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for x in range(1, 101):
         if (x % 15 == 0):
-            print("FizzBuzz ")
+            print("{}".format("FizzBuzz "), end="")
         elif (x % 5 == 0):
-            print("Buzz ")
+            print("{}".format("Buzz "), end="")
         elif (x % 3 == 0):
-            print("Fizz ")
+            print("{}".format("Fizz "), end="")
         else:
-            print("{}".format(x))
+            print("{}".format(x), end " ")
     print()
