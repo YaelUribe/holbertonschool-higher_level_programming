5f1bead7d528dff80f23121b1e106eca8596313c
Yaeluribe <2962@holbertonschool.com>
2021-04-27 20:31:52 -0500

    Check for lowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,6 @@ def islower(c):
     """function to check lowercases"""
     x = ord(c)
     if 96 < x < 123:
-        return (True)
+        return True
     else:
-        return (False)
+        return False
