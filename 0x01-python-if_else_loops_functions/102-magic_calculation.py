9d4e3041188bc812b9fa2cf39a872fdfc62c7fdd
Yaeluribe <2962@holbertonschool.com>
2021-04-27 22:57:20 -0500

    Magic calculation

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(x, y, z):
+    if x < y:
+        return z
+    else:
+        if z > y:
+            return x + y
+        else:
+            return x * y - z
