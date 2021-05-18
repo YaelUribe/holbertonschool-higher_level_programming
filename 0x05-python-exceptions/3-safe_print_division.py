9b605346f030aa712acf53509ddf9d3cf4e53fbf
Yaeluribe <2962@holbertonschool.com>
2021-05-18 14:09:49 -0500

    Printig a division result, handling Zero division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
         total = a / b
     except ZeroDivisionError:
         total = None
-        return total
+        return None
     finally:
         print("Inside reult: {}".format(total))
     return total
