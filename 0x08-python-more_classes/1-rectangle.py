9880e33f7193c300a41aac26bf928289ebaf941e
Yaeluribe <2962@holbertonschool.com>
2021-05-20 17:15:21 -0500

    Defining and setting properties

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+
+	def __init__(self, width=0, height=0):
+		self.__width = width
+		self.__height = height
+
+	@property
+	def width(self):
+		return self.__width
+
+	@width.setter
+	def width(self, value):
+		self.__width = value
+		if value < 0 :
+			raise TypeError("width must be >= 0")
+		elif type(value) != int:
+			raise ValueError("width must be an integer")
+
+	@property
+	def height(self):
+		return self.__height
+
+	@height.setter
+	def height(self, value):
+		self.__height = value
+		if value < 0 :
+			raise TypeError("height must be >= 0")
+		elif type(value) != int:
+			raise ValueError("height must be an integer")
