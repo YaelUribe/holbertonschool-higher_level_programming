47f308bf1b57eaf7a85b70f552356099c6017385
Yaeluribe <2962@holbertonschool.com>
2021-05-20 17:26:18 -0500

    Started methods an documentated them

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        elif type(value) != int:
+            raise ValueError("width must be an integer")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        elif type(value) != int:
+            raise ValueError("height must be an integer")
+
+	def area(self):
+		"""Method to determine area of our rectangle"""
+
+	def perimeter(self):
+		"""Method to determine perimeter of our rectangle"""
