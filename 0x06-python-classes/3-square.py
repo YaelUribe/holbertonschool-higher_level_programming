b8d37e8662b5c280a89959aaa11430597955e5ab
Yaeluribe <2962@holbertonschool.com>
2021-05-19 19:26:40 -0500

    Printing the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square Class to define a square"""
+
+
+class Square:
+    """Class: Square"""
+    def __init__(self, size=0):
+        """Defining private attributes"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Method to calculate area"""
+        self.__size = self.__size * self.__size
+        return self.__size
