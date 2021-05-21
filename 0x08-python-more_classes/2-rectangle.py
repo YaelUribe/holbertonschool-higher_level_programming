6b4f93b874220da6e5e0d4090a408818913ee774
Yaeluribe <2962@holbertonschool.com>
2021-05-20 19:30:59 -0500

    Corrected PEP8

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,16 +8,17 @@ class Rectangle:
         """Defining private attributes"""
         self.width = width
         self.height = height
+
     @property
     def width(self):
         return self.__width
 
     @width.setter
     def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        elif type(value) != int:
-            raise TypeError("width must be an integer")
         self.__width = value
 
     @property
@@ -26,10 +27,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        elif type(value) != int:
-            raise TypeError("height must be an integer")
         self.__height = value
 
     def area(self):
