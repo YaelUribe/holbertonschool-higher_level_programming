4bbae2e8b53351dd03839dd56612e4ba7ff39f2b
Yaeluribe <2962@holbertonschool.com>
2021-09-08 15:18:12 -0500

    Added line to print the string in args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(args - 1))
     elif args > 2:
         print("{} arguments:".format(args - 1))
+    for x in range(1, args):
+        print("{}: {}".format(x, sys.argv[x]))
