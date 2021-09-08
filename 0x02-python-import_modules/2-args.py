a4dd997a503c428aa95041b5846ce5582c89b0cb
Yaeluribe <2962@holbertonschool.com>
2021-09-08 15:16:34 -0500

    Determining amount of args given

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,3 @@ if __name__ == "__main__":
         print("{} argument:".format(args - 1))
     elif args > 2:
         print("{} arguments:".format(args - 1))
-    for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))
