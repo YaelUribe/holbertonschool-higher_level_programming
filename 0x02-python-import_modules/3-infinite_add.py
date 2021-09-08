16fa9fc61bf0f6240e293f4c981c9bc4260593f2
Yaeluribe <2962@holbertonschool.com>
2021-09-08 15:22:12 -0500

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    total = 0
+    result = 0
     argc = len(sys.argv)
     for x in range(1, argc):
-        total += int(sys.argv[x])
-    print("{}".format(total))
+        result += int(sys.argv[x])
+    print("{}".format(result))
