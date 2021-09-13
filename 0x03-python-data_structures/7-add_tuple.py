6cdec6282d49e0a2ff4fa1594acc91e367395561
Yaeluribe <2962@holbertonschool.com>
2021-09-13 14:23:04 -0500

    the resulting tuple has to be declared at once, because then becomes immutable

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a, tuple_b):
-	i = len(tuple_a)
-	b = len(tuple_b)
-	if i > 0 and b > 0:
-		tuple_total[0]= (tuple_a[0] + tuple_b[0])
-
-	if i > 1 and b > 1:
-		tuple_total[1] = (tuple_a[1] + tuple_b[1])
-
-		return tuple_total
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = len(tuple_a)
+    b = len(tuple_b)
+    tuple_add = ((tuple_a[0] if i > 0 else 0) + (tuple_b[0] if b > 0 else 0),
+                 (tuple_a[1] if i > 1 else 0) + (tuple_b[1] if b > 1 else 0))
+    return tuple_add
