fd726391d177aa0a383e832aa926edaaf917e6e4
Yaeluribe <2962@holbertonschool.com>
2021-09-13 15:32:57 -0500

    deleting using del function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        del my_list[idx]
+        for i in my_list:
+            if idx == i:
+                del my_list[i]
     return my_list
