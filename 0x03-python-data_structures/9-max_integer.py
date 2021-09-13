8dba321feb086dd364a03ad1c2cf6151163ce883
Yaeluribe <2962@holbertonschool.com>
2021-09-13 14:57:09 -0500

    used sort() to print the last integer that sould be the max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     length = len(my_list)
-    if length == 0:
+    if length <= 0:
         return None
-    my_list.sort()
+    else:
+        my_list.sort()
     return my_list[-1]
