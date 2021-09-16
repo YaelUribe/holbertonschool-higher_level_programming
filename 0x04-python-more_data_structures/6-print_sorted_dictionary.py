89b340958a721a7013eefb2a726bbf1ccd120141
Yaeluribe <2962@holbertonschool.com>
2021-09-15 21:58:20 -0500

    print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    copy = a_dictionary.copy()
-    for x, y in sorted(copy.items()):
-        print("{}: {}".format(x, y))
+    for i, j in sorted(a_dictionary.items()):
+        print("{}: {}".format(i, j))
