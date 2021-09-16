fc7353d2d19b65776193531cd832f594324a2a20
Yaeluribe <2962@holbertonschool.com>
2021-09-15 20:29:11 -0500

    corrected repep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     nu_list = matrix.copy()
-	return [list(map((lambda x: x * x), i)) for i in nu_list]
+    return [list(map((lambda x: x * x), i)) for i in nu_list]
