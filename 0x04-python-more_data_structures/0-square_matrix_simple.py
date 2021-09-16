5d538f2d7988c66f6905924369752b9b5f63c82d
Yaeluribe <2962@holbertonschool.com>
2021-09-15 20:24:59 -0500

    something doesn't work

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    copy = matrix.copy()
-    return [list(map((lambda x: x * x), i)) for i in copy]
+def square_matrix_simple(matrix):
+	nu_list = matrix.copy()
+	[list(map((lambda x: x * x), i)) for i in nu_list]
+	return nu_list
