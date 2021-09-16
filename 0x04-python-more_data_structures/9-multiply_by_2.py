92f511d238bb25d360dec07dcac854fc59297054
Yaeluribe <2962@holbertonschool.com>
2021-09-15 22:37:21 -0500

    x * 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     copy = {}
-    for x, y in a_dictionary.items():
-        copy[x] = y * 2
+    for i, j in a_dictionary.items():
+        copy[i] = j * 2
     return copy
