79896e273c239f199948823b829c05b7a240aa12
Yaeluribe <2962@holbertonschool.com>
2021-09-15 20:50:11 -0500

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     copy = []
-    for x in my_list:
-        if x is search:
-            copy.append(replace)
-        else:
-            copy.append(x)
-    return copy
+    nu_list = [list(map((lambda x: x=replace), if i == search) for i in nu_list)]
