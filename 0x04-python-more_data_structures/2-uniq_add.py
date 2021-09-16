31b5e6477647dd462a8fee19dba29b148413e376
Yaeluribe <2962@holbertonschool.com>
2021-09-15 21:21:01 -0500

    corrected pep8

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list):
-    uniq= []
+    uniq = []
     for x in my_list:
         if x not in uniq:
             uniq.append(x)
-    y= 0
+    y = 0
     for i in uniq:
         y += i
     return y
