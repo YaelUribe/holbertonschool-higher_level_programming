239e4ad2f15522196d46805b49b1ebf888059a6a
Yaeluribe <2962@holbertonschool.com>
2021-09-27 16:07:09 -0500

    safe division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         total = None
         return None
-    finally:
-        print("Inside result: {}".format(total))
     return total
