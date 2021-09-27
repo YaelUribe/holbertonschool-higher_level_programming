05ca4b188f3aeacad8aa4ac862dee7eb759b5910
Yaeluribe <2962@holbertonschool.com>
2021-09-27 16:34:22 -0500

    Function to be executed safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         total = fct(*args)
         return total
-    except Exception as x:
-        print("Exception: {}".format(x), file=sys.stderr)
+    except Exception as y:
+        print("Exception: {}".format(y), file=sys.stderr)
         return None
