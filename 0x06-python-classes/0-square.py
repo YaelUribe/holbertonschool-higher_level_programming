a3b188656795dea2079f6a891ecfa4e77d124b91
Yaeluribe <2962@holbertonschool.com>
2021-09-28 13:41:04 -0500

    redoing the project

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Empty class to define a Square"""
-
-
-class Square:
-    """Empty class Square"""
-    pass  # Empty block
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Square Class to define a square"""
-
-
-class Square:
-    """Class: Square"""
-    def __init__(self, size):
-        self.__size = size
