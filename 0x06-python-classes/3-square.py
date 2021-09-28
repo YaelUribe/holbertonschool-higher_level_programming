5692bb0e2bcbac46c732d47c5510a85928d86bbb
Yaeluribe <2962@holbertonschool.com>
2021-09-28 14:10:05 -0500

    Defined public method to define the area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,4 +10,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-def area(self):
+
+    def area(self):
+        """Public Method to calculate area of a Square"""
+        self.__size *= self.__size
+        return self.__size
