dd40b7bceac041badff253a9f40ab7e31281112d
Yaeluribe <2962@holbertonschool.com>
2021-09-28 16:13:03 -0500

    corrected pep8 and verified result by complementing my_print function

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,8 +15,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         for index in position:
             if index < 0 or type(index) is not int:
-                raise TypeError("position must be a tuple of 2 positive integers")
-    
+                raise TypeError("position must be a tuple of\
+                    2 positive integers")
+
     def area(self):
         """Public Method to calculate area of a Square"""
         return self.__size * self.__size
@@ -24,6 +25,8 @@ class Square:
     @property
     def size(self):
         return self.__size
+
+    @property
     def position(self):
         return self.__position
 
@@ -39,17 +42,23 @@ class Square:
     def position(self, value):
         self.__position = value
         if (len(value) != 2) or type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2\
+                positive integers")
         for index in value:
             if index < 0 or type(index) is not int:
-                raise TypeError("position must be a tuple of 2 positive integers")
+                raise TypeError("position must be a tuple of\
+                    2 positive integers")
 
     def my_print(self):
         """Public instance Method to print a Square"""
         if self.__size == 0:
             print()
         else:
+            for tup0 in range(self.__position[1]):
+                print()
             for x in range(self.__size):
+                for tup1 in range(self.__position[0]):
+                    print(" ", end="")
                 for y in range(self.__size):
                     print("#", end="")
                 print()
