69490601baa544d3fe61dfa654bf1fb6296a4bf3
Yaeluribe <2962@holbertonschool.com>
2021-09-28 15:04:03 -0500

    Coordinates of a square based on 5-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Class to define a Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public Method to calculate area of a Square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        """Public instance Method to print a Square"""
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end="")
+                print()
