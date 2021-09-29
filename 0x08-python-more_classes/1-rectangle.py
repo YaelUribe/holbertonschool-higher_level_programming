04173b7aed7df6037c1e01e4ab5137630760f55b
Yaeluribe <2962@holbertonschool.com>
2021-09-29 17:22:11 -0500

    changed attributes to public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,9 +6,8 @@ class Rectangle:
     """Class rectangle instantiation"""
     def __init__(self, width=0, height=0):
         """Private attributes"""
-        self.__width = width
-        self.__height = height
-
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
