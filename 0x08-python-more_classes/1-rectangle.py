ddb6ed2777948b3ead2f40d57378a5b2bf5c879d
Yaeluribe <2962@holbertonschool.com>
2021-09-29 17:16:15 -0500

    moved statements in setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,33 +6,33 @@ class Rectangle:
     """Class rectangle instantiation"""
     def __init__(self, width=0, height=0):
         """Private attributes"""
-        self.__height = height
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """width properties"""
         return self.__width
 
+    @width.setter
+    def width(self, value):
+        """"Set width properties"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     @property
     def height(self):
         """height properties"""
         return self.__height
 
-    @width.setter
-    def width(self, value):
-        """"Set width properties"""
-        self.__width = value
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-
     @height.setter
     def height(self, value):
         """set height properties"""
-        self.__height = value
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
