0e2d2e9fdff2b53838dad4f78c00a5a704e4b8bf
Yaeluribe <2962@holbertonschool.com>
2021-09-29 18:28:34 -0500

    __del__ when using del to delete an object/instance

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,1 +1,70 @@
 #!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """Class rectangle instantiation"""
+    def __init__(self, width=0, height=0):
+        """Private attributes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """height properties"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height properties"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """width properties"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """"Set width properties"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """area of a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """perimeter of a rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return Class objects as strings"""
+        new = ""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        for h in range(self.__height):
+            for w in range(self.__width):
+                new += "#"
+            if h == self.__height - 1:
+                break
+            new += "\n"
+        return new
+
+    def __repr__(self):
+        """__repr__ to print a string representation of a rectangle"""
+        return "Rectangle(" + str(self.__width) + ", "\
+            + str(self.__height) + ")"
+
+    def __del__(self):
+        """Object destructor, when using del()"""
+        print("Bye rectangle...")
