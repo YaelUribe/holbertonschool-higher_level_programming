2de30a72bdeedfb6b692fd55ff793c059c9e3677
Yaeluribe <2962@holbertonschool.com>
2021-09-29 17:09:48 -0500

    docstring corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,19 +5,23 @@
 class Rectangle:
     """Class rectangle instantiation"""
     def __init__(self, width=0, height=0):
-        self.__height = height
+        """Private attributes"""
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
+        """width properties"""
         return self.__width
 
     @property
+    """height properties"""
     def height(self):
         return self.__height
 
     @width.setter
     def width(self, value):
+        """"Set width properties"""
         self.__width = value
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -26,6 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """set height properties"""
         self.__height = value
         if type(value) != int:
             raise TypeError("height must be an integer")
