34663a1e50ac7d3ad215f488bf55f9b618eb1e28
Yaeluribe <2962@holbertonschool.com>
2021-09-29 17:12:08 -0500

    changed attribute lines

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Class rectangle instantiation"""
     def __init__(self, width=0, height=0):
         """Private attributes"""
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
