40c6ef277bed02a12cfd09dd63b43259a4c31ab1
Yaeluribe <2962@holbertonschool.com>
2021-09-29 18:51:36 -0500

    Comparing 2 rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -73,3 +73,15 @@ class Rectangle:
         """Object destructor, when using del()"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Are 2 rectangles equal??"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
