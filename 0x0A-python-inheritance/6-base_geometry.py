b46acc2f1a3be1b431fa23145ebdf782f53da2f0
Yaeluribe <2962@holbertonschool.com>
2021-10-11 15:14:35 -0500

    def area() function with an Exceptin message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module - class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+    def area(self):
+        """
+        function to define an area
+        meanwhile raises an Exception
+        """
+        raise Exception("area() is not implemented")
