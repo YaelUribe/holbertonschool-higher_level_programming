eae50b9882d6144c027af713df38c389442f93b8
Yaeluribe <2962@holbertonschool.com>
2021-10-11 17:20:35 -0500

    switching values for == and !=

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+method: class MyInt
+"""
+
+
+class MyInt(int):
+	"""
+	Class MyInt
+	inherits from int
+	"""
+	def __init__(self, i):
+		self.__i = i
+	
+	def __ne__(self, other):
+		return not self == other
