1daf6c6b79598280083d24dfac9a4bf7b77f3aca
Yaeluribe <2962@holbertonschool.com>
2021-10-11 19:07:59 -0500

    modified statement

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,8 +9,7 @@ def inherits_from(obj, a_class):
         of a class that inherited directly
         or indirectly from a_class
     """
-    if isinstance(obj, a_class):
-        if type(obj) is not a_class:
-            return True
+    if isinstance(obj, a_class) is True and type(obj) is not a_class:
+        return True
     else:
         return False
