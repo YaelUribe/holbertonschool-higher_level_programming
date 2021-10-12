df686bc52ffc5689bebba9d8e2e8963f68a2e215
Yaeluribe <2962@holbertonschool.com>
2021-10-12 17:38:59 -0500

    added empty string to the end

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Module - read_file
 def read_file(filename=""):
     """Read file method, to stdout from utf-8 encoding"""
     with open(filename, 'r', encoding="utf8") as f:
-        print(f.read())
+        print(f.read(), end="")
