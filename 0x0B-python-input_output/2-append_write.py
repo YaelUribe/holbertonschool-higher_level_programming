2f3f824b38b8a5bd8cd0a958976f4b6f1ba804a3
Yaeluribe <2962@holbertonschool.com>
2021-10-12 17:11:41 -0500

    verification of str type

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,5 @@ Module: append_write
 
 def append_write(filename="", text=""):
     with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+        str1 = str(text)
+        return f.write(str1)
