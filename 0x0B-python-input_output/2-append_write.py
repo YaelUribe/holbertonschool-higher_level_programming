6f2e8309d7f0e08b5ccbe653c48c99bff1497905
Yaeluribe <2962@holbertonschool.com>
2021-10-12 17:27:34 -0500

    rewritten utf8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,5 @@ Module: append_write
 
 
 def append_write(filename="", text=""):
-    with open(filename, 'a', encoding="utf-8") as f:
-        total = f.write(text)
-        return total
+    with open(filename, 'a', encoding="utf8") as f:
+        return f.write(text)
