e8094fa06cfbdbb5f79ce3d4b9e2f028b3b68f71
Yaeluribe <2962@holbertonschool.com>
2021-10-12 17:37:18 -0500

    rewritten utf8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Module - read_file
 
 def read_file(filename=""):
     """Read file method, to stdout from utf-8 encoding"""
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf8") as f:
         print(f.read())
