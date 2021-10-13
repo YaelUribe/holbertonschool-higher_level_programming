7b257e3f359ecc1788a811dc7f10ef1f8910a76d
Yaeluribe <2962@holbertonschool.com>
2021-10-13 13:35:35 -0500

    Class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module: student.py
+"""
+
+
+class Student():
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """ instantiation for class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Method to retrieve a dictionary representation
+        of a Student instance
+        """
+        return dict(self)
