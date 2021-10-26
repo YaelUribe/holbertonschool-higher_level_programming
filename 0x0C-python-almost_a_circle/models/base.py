be28d24ea5761c735934189f145c10496193c12e
Yaeluribe <2962@holbertonschool.com>
2021-10-25 19:23:26 -0500

    static method from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,8 +63,8 @@ class Base:
         all attributes already set
         """
         if cls.__name__ == "Rectangle":
-            dummy = cls(1)
-        elif cls.__name__ == "Square":
             dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
