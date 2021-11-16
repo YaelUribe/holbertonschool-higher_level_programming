af23455d4ef0b7a89dcbafdf9697ee1d7917d9f8
Yaeluribe <2962@holbertonschool.com>
2021-11-16 18:49:16 -0500

    average temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- AVG temperatures by city
-SELECT city, AVG(value) AS avg_temp GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
