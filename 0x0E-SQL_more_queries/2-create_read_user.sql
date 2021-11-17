77b9c1e53bafe38813d4587984e9b6bf02e7ab27
Yaeluribe <2962@holbertonschool.com>
2021-11-17 18:54:29 -0500

    Create READ users

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Read user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' TO 'user_0d_2_pwd';
-GRANT SELECT ONLY ON hbtn_0d_2.*;
+GRANT SELECT ONLY ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
