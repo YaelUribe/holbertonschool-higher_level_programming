97567cb3da5296df9f25260ae5efbf9600a1a9fd
Yaeluribe <2962@holbertonschool.com>
2021-12-13 18:42:17 -0500

    fixed codestyle

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,12 +4,10 @@ require('process');
 
 const args = process.argv;
 
-if (args.length === 2){
-	console.log('No argument');
-}
-else if (args.length === 3){
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
