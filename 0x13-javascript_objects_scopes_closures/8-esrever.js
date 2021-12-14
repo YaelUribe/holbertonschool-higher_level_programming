8914b6d243cfb126ad429fd47a8608900955a9fc
Yaeluribe <2962@holbertonschool.com>
2021-12-14 16:03:58 -0500

    task8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+	const reverser = [];
+	for (const i in list) {
+	  if (list[i] === searchElement) {
+		counter[i] = list[i];
+	  }
+	}
+	return counter;
+  };
+  
