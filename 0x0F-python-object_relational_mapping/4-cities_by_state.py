c0327a047dfd636d8441e7a54f8a106e78cf90eb
Yaeluribe <2962@holbertonschool.com>
2021-12-20 12:08:50 -0500

    Listing cities by states, was missing states.name

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         db=db_name)
 
     cursor = db.cursor()  # create a cursor
-    cursor.execute("""SELECT cities.id, cities.name
+    cursor.execute("""SELECT cities.id, cities.name, states.name
                     FROM cities JOIN states
                     ON cities.state_id = states.id
                     ORDER BY cities.id""")
