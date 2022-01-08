0e28a2c00e0bc85a00865085a2502e0b85fd5bb7
YaelUribe <yael3x@gmail.com>
2022-01-08 18:37:48 -0500

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Script to take in an URL and send a POST request"""
+import sys
+from urllib import parse, request
+
+if __name__ == "__main__":
+    email = sys.argv[2]
+    content = parse.urlencode({'email': email}).encode('utf-8')
+    with request.urlopen(sys.argv[1], content) as response:
+        html = response.read()
+        html = response.decode('utf-8')
+    print(html)
+
