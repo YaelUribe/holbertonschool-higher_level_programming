bb6dead88635448899ca526dde2d2a91d011921c
YaelUribe <yael3x@gmail.com>
2022-01-08 17:01:55 -0500

    if I don't have to parse better use sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,9 +7,5 @@ def find_peak(list_of_integers):
     lenght = len(list_of_integers)
     if (lenght == 0):
         return None
-    for i in range(lenght - 1):
-        if (i == 0):
-            i += 1
-        elif (list_of_integers[i - 1] <= list_of_integers[i] and
-              list_of_integers[i + 1] <= list_of_integers[i]):
-            print("{}".format(list_of_integers[i]))
+    list_of_integers.sort()
+    return list_of_integers[-1]
