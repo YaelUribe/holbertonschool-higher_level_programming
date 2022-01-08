6c92e531f0e8a4146b8c9a5cf6b25f6ff5287edf
YaelUribe <yael3x@gmail.com>
2022-01-08 18:25:43 -0500

    Getting header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Script to fetch an URL and displaying the value of given variable"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("{}".format(sys.argv[1])) as response:
+        html = response.read()
+        for key in response.__dict__:
+            value = response.headers.get('X-Request-Id')
+        print(value)
