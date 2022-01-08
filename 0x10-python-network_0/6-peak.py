8eb595f530cf26a8aed1f96f0989da4c6a42266b
YaelUribe <yael3x@gmail.com>
2022-01-08 16:57:45 -0500

    ??.. close but it says there no need of parsing the list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,12 +5,11 @@
 def find_peak(list_of_integers):
     """Funcion to find a Peak in an unsorted list"""
     lenght = len(list_of_integers)
-    if (len <= 1):
+    if (lenght == 0):
         return None
     for i in range(lenght - 1):
-        if (list_of_integers[i - 1] == NULL):
+        if (i == 0):
             i += 1
         elif (list_of_integers[i - 1] <= list_of_integers[i] and
               list_of_integers[i + 1] <= list_of_integers[i]):
-            print("{}".format(list_of_integers[i]) end=",")
-    print("\n")
+            print("{}".format(list_of_integers[i]))
