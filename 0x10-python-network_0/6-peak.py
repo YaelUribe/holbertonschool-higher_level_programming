18fbe8bdcabdc8ea47b57ce6a09a7ed0f91ea9e3
YaelUribe <yael3x@gmail.com>
2022-01-08 16:48:47 -0500

    this doesn't seem like working

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """Module: peak"""
 
-from typing import List
-
 
 def find_peak(list_of_integers):
     """Funcion to find a Peak in an unsorted list"""
     lenght = len(list_of_integers)
-    peak_s = ""
     if (len <= 1):
         return None
     for i in range(lenght - 1):
         if (list_of_integers[i - 1] == NULL):
             i += 1
-        if (list_of_integers[i] == list_of_integers[lenght]):
-            
-        
+        elif (list_of_integers[i - 1] <= list_of_integers[i] and
+              list_of_integers[i + 1] <= list_of_integers[i]):
+            print("{}".format(list_of_integers[i]) end=",")
+    print("\n")
